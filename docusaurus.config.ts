1dde67a0b79db8b83cd3b744e6dd32026cd53202
zriyanshdz <priyansh@datazip.io>
2025-01-28 15:43:48 +0530

    fixed broken links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -98,7 +98,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/',
             },
           ],
         },
