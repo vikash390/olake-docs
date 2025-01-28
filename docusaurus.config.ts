cc51812408ef64ae15a396dba8a13cb65da76c8b
zriyanshdz <priyansh@datazip.io>
2025-01-28 20:00:33 +0530

    update base path and url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/logo/olake.png',
 
   // Set the production url of your site here
-  url: 'https://olakedocs.datazip.io/',
+  url: 'https://datazip-inc.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'olake/docs',
+  baseUrl: 'olake-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
