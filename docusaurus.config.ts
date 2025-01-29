cd724a941966128e0bf1d6159a1b1ecfe76d4a5c
zriyanshdz <priyansh@datazip.io>
2025-01-29 16:21:38 +0530

    update getting started guide

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/datazip-inc/olake-docs/',
+            'https://github.com/datazip-inc/olake-docs/tree/master/',
         },
         blog: false
       } satisfies Preset.Options,
