82bab3dc9009e3379ab1e1868ecbb5cbe449b01e
zriyanshdz <priyansh@datazip.io>
2025-02-07 15:03:27 +0530

    fix themeing

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,7 +24,6 @@ const config: Config = {
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
   trailingSlash: false,
-  
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
