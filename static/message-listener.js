160122e52f44e7840b0760398b7d995ac724de08
zriyansh_dz <priyansh@datazip.io>
2025-04-16 13:49:10 +0530

    update docs with troubleshooting guide and listener script

--- static/message-listener.js
+++ static/message-listener.js
@@ -2,6 +2,7 @@
 window.addEventListener("message", (event) => {
     if (event.data?.theme) {
       document.documentElement.setAttribute("data-theme", event.data.theme);
+      console.log("Theme changed to:", event.data.theme);
     }
   });
   
