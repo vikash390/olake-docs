7c8596ea895351d128769de6001e24943173d879
Taraka Swathi <swathi@datazip.io>
2025-07-08 12:58:17 +0530

    chore: update announcementBar (#53)
    * feat:
    
    * feat: remove announcement bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -282,14 +282,6 @@ const config = {
         },
       ],
 
-      announcementBar: {
-        content:
-          '<strong>OLake launch happening 5th July 2025</strong>&nbsp;&nbsp;&nbsp;<a target="_blank" rel="noopener noreferrer" href="https://lu.ma/8af1z837" style="color: white; font-weight: bold;">REGISTER NOW</a>',
-        backgroundColor: '#193AE6',
-        textColor: 'white',
-        isCloseable: true,
-      },
-
       colorMode: {
         defaultMode: "light", // dark or light
         disableSwitch: false,
